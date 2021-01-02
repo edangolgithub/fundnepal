110cc75411255c808b2ccccbd4f10614cfbcd72a
edangolgithub <dangolevan@gmail.com>
2021-01-02 05:59:28 +0000

    web app init

---
+++ dotnetmain/evanconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace evanconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello evan!");
+        }
+    }
+}
