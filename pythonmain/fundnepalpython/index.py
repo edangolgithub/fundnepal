6aef4d919aafff8d35a7d3e2328043c11921497c
edangolgithub <dangolevn@gmail.com>
2021-01-06 04:01:03 +0000

    ok

--- pythonmain/fundnepalpython/index.py
+++ pythonmain/fundnepalpython/index.py
@@ -2,16 +2,46 @@ from flask import Flask
 import requests
 import json
 
+import pandas as pd
+import pymysql
+
 app = Flask(__name__)
+
+@app.route('/data')
+def data():
+    host="fundnepaldbcluster.cluster-cjiabok62vq8.us-east-1.rds.amazonaws.com"
+    port=3306
+    dbname="fundnepaldb"
+    user="root"
+    password="mysqlpassword"
+
+    con = pymysql.connect(host, user=user,port=port,
+                           passwd=password, db=dbname)
+    try:
+
+     with con.cursor() as cur:
+
+      cur.execute('SELECT VERSION()')
+
+      version = cur.fetchone()
+
+      print(f'Database version: {version[0]}')
+      print(con.server_version)
+
+    finally:
+      con.close()     
+      return "ok"
+
 @app.route('/')
 def hello_world():
     
     api_url_base = "http://api.open-notify.org/astros.json"
-    
+    api="https://sa87fhffjj.execute-api.us-east-1.amazonaws.com/Stage"
 
-    response = requests.get(api_url_base)
-
+   #response = requests.get(api)
+    response=requests.get(api_url_base)
     if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
+        #return response.content
+        return response.json();
     else:
         return None
