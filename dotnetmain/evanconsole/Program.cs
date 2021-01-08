61766d6fdedf60bddb713ec2ec76f2cbd1f5788d
edangolgithub <dangolevn@gmail.com>
2021-01-08 16:25:56 +0000

    uytg

--- dotnetmain/evanconsole/Program.cs
+++ dotnetmain/evanconsole/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
 
 namespace evanconsole
 {
     class Program
     {
+        public static Table persontable;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello evan!");
+            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("Hello, World!"));
+            dynamo1();
         }
+        public static void run()
+        {
+
+        }
+        public async static void dynamo1()
+        {
+            AmazonDynamoDBConfig ddbConfig = new AmazonDynamoDBConfig();
+          //  ddbConfig.ServiceURL = "https://dynamodb.us-east-1.amazonaws.com";
+            ddbConfig.RegionEndpoint = RegionEndpoint.
+            Document pdocument = new Document();
+            AmazonDynamoDBClient client = new AmazonDynamoDBClient(ddbConfig);
+            Task<Document> readMovie = persontable.GetItemAsync(1);
+            pdocument = await readMovie;
+        }
+
     }
 }
