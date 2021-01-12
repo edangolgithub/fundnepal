a2949c6732b68085dbae444e99657c8bf850dc0b
edangolgithub <dangolevn@gmail.com>
2021-01-12 08:22:24 +0000

    ok

--- dotnetmain/fundnepalnew/fundnepalnew/Controllers/HomeController.cs
+++ dotnetmain/fundnepalnew/fundnepalnew/Controllers/HomeController.cs
@@ -3,15 +3,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace fundnepalnew
 {
+ [Route("home")]
+
     public class HomeController : Controller
     {
         [Authorize]
        
-        [Route("home/index")]
-        public IActionResult Index()
+               public IActionResult Index()
         {
             
             return View("Home");
+        }
+        
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 
