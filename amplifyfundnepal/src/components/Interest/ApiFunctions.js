113819a30093e2c73a88fb27af766786a4e176b5
edangolgithub <dangolevn@gmail.com>
2021-03-16 15:47:36 +0000

    gcvgfcvf

--- amplifyfundnepal/src/components/Interest/ApiFunctions.js
+++ amplifyfundnepal/src/components/Interest/ApiFunctions.js
@@ -1,78 +1,82 @@
 import axios from 'axios'
-import {resolveaccounttype} from './Calculation'
+import { resolveaccounttype } from './Calculation'
 import { assert, presence, email } from 'property-validator';
 export async function getaccounttypes() {
-   
-  const data= await axios.get('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/accounttype')
-       return data;
-   
+
+  const data = await axios.get('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/accounttype')
+  return data;
+
 }
 export async function getaccounts() {
-   
-  const data=  axios.get('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/account')
-        return data;      
-   
+
+  const data = axios.get('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/account')
+  return data;
+
 }
 export async function gettransactions() {
-   
-    const data= await axios.get('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/transaction')
-       return data;
+
+  const data = await axios.get('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/transaction')
+  return data;
 }
 export function postnewaccount(newaccount) {
-    //.log(newaccount)
-    //console.log(resolveaccounttype(newaccount.accounttypeid))
-    let user = {
-        username: 'nettofarah',
-        email_address: 'invalid@email'
-      }
-var res= assert(newaccount, [
-        presence('accountid'),
-        email('email'),
-        presence('amount'),
-        presence('accounttypeid')
-      ]);
-      if (!res.valid) {
-        return;
+  //.log(newaccount)
+  //console.log(resolveaccounttype(newaccount.accounttypeid))
+  let user = {
+    username: 'nettofarah',
+    email_address: 'invalid@email'
+  }
+  var res = assert(newaccount, [
+    presence('accountnumber'),
+    email('email'),
+    presence('amount'),
+    presence('accounttypeid')
+  ]);
+  if (!res.valid) {
+    return;
+  } else {
+    alert("form is ok")
+  }
+
+
+
+  var rt = resolveaccounttype(newaccount.accounttypeid);
+  var d = new Date().toISOString();
+
+
+
+  axios.post('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/account', {
+    created: d,
+    interest: 0,
+    isenabled: "1",
+    address: newaccount.address,
+    accountid: newaccount.accountid,
+    accounttypeid: newaccount.accounttypeid,
+    amount: parseFloat(newaccount.amount),
+    accountname: newaccount.accountname,
+    balance: parseFloat(newaccount.amount),
+    phone: newaccount.phone,
+    email: newaccount.email,
+    citizenshipno: "110892",
+    addressline1: "Dathu Sadak , Khusibu",
+    addressline2: "hehe",
+    city: "Kathmandu",
+    country: "Nepal",
+    mobile: "9849178036",
+    phone1: "9849178036",
+    accountnumber: "b9dab5a0-d765-4ede-bb0f-b1adbebdb800"
+
+  })
+    .then(() => alert("success"))
+    .catch(err => {
+      if (err.response) {
+        // client received an error response (5xx, 4xx)
+        alert(err)
+      } else if (err.request) {
+        // client never received a response, or request never left
+        alert(err)
       } else {
-        alert("form is ok")
+        // anything else
+        alert(err)
       }
-    
-      return;
-
-    var rt=resolveaccounttype(newaccount.accounttypeid);  
-    var d = new Date().toISOString() ;
-
-
-    
-    axios.post('https://nxopo5t28l.execute-api.us-east-1.amazonaws.com/Prod/api/account', {
-        created: d,        
-        islatest: "1",
-        interest: 0,
-        type: "cash",
-        entry: "debit",
-        accounttype: rt,
-        isenabled: "1",
-        address: newaccount.address,
-        accountid: newaccount.accountid,
-        accounttypeid: newaccount.accounttypeid,
-        amount: parseFloat(newaccount.amount),
-        accountname: newaccount.accountname,
-        balance:parseFloat(newaccount.amount),
-        phone: newaccount.phone,
-        email: newaccount.email,
-       
     })
-        .then(() => alert("success"))
-        .catch(err => {
-            if (err.response) {
-                // client received an error response (5xx, 4xx)
-                alert(err)
-            } else if (err.request) {
-                // client never received a response, or request never left
-                alert(err)
-            } else {
-                // anything else
-                alert(err)
-            }
-        })
 }
--- amplifyfundnepal/src/components/Interest/NewAccount.js
+++ amplifyfundnepal/src/components/Interest/NewAccount.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import * as fun from './ApiFunctions'
+import { x } from "./Province"
+
 
 const validEmailRegex = RegExp(
   /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
@@ -12,7 +15,9 @@ const validateForm = errors => {
 
 export class NewAccount extends Component {
   constructor(props) {
+
     super(props);
+
     this.state = {
       data: [],
       d: [],
@@ -20,12 +25,74 @@ export class NewAccount extends Component {
         accountid: '',
         email: '',
         password: '',
-      }
+      },
+      districts: x.Bagmati,
+      selectedFile: null
     }
     this.handleChange = this.handleChange.bind(this);
     this.addAccount = this.addAccount.bind(this);
+    this.handleprovincechange = this.handleprovincechange.bind(this)
+    this.onFileChange = this.onFileChange.bind(this)
+    this.onFileUpload = this.onFileUpload.bind(this)
   }
+  // On file select (from the pop up)
+  onFileChange = event => {
 
+    // Update the state
+    this.setState({ selectedFile: event.target.files[0] });
+    console.log(this.state.selectedFile);
+  };
+
+  // On file upload (click the upload button)
+  onFileUpload = () => {
+
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append(
+      "myFile",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+
+    // Details of the uploaded file
+    console.log(this.state.selectedFile);
+
+    // Request made to the backend api
+    // Send formData object
+    axios.post("/upload", formData);
+  };
+
+  handleprovincechange(event) {
+
+    // this.setState({ districts: [] });
+    switch (event.target.value) {
+      case "Province1":
+        this.setState({ districts: x.Province1 });
+        break;
+      case "Province2":
+        this.setState({ districts: x.Province2 });
+        break;
+      case "Bagmati":
+        this.setState({ districts: x.Bagmati });
+        break;
+      case "Gandaki Province":
+        this.setState({ districts: x['Gandaki Province'] });
+        break;
+      case "Province5":
+        this.setState({ districts: x.Province5 });
+        break;
+      case "Karnali Province":
+        this.setState({ districts: x['Karnali Province'] });
+        break;
+      case "Sudurpaschim Province":
+        this.setState({ districts: x['Sudurpaschim Province'] });
+        break;
+
+    }
+
+  }
   handleChange(evt) {
     var fname = evt.target.name;
     var fvalue = evt.target.value;
@@ -65,6 +132,8 @@ export class NewAccount extends Component {
 
   addAccount(event) {
     event.preventDefault();
+    //console.log(selectedFile)
+
     if (validateForm(this.state.errors)) {
       alert('Valid Form')
       return
@@ -82,6 +151,11 @@ export class NewAccount extends Component {
       <div className="container jumbotron m-5">
         <form onSubmit={this.addAccount}>
           <div className="form-group">
+            <label htmlFor="phone">upload</label>
+            <input type="file" name="upload" onChange={this.onFileChange} className="form-control" id="upload" placeholder="upload" />
+            <input type="button" onClick={this.onFileUpload} value="upload" className="btn btn-primary" />
+          </div>
+          <div className="form-group">
             <label htmlFor="accountnumber">Account Number</label>
             <input type="text" name="accountid" onChange={this.handleChange} className="form-control" id="accountnumber" placeholder="Account Number" />
             {errors.accountid.length > 0 &&
@@ -89,6 +163,11 @@ export class NewAccount extends Component {
           </div>
           <div className="form-group">
             <label htmlFor="accountname">Account Name</label>
+            <input type="text" name="firstname" onChange={this.handleChange} className="form-control" id="nomineefirstname" placeholder="First Name" />
+            <input type="text" name="middlename" onChange={this.handleChange} className="form-control" id="nomineemiddlename" placeholder="Middle Name" />
+            <input type="text" name="lastname" onChange={this.handleChange} className="form-control" id="nomineelastname" placeholder="Last Name" />
+            <input type="text" name="citizennumber" onChange={this.handleChange} className="form-control" id="nomineecitizennumber" placeholder="Citizenship Number" />
+
             <input type="text" name="accountname" onChange={this.handleChange} className="form-control" id="accountname" placeholder="Account Name" />
           </div>
           <div className="form-group">
@@ -96,16 +175,76 @@ export class NewAccount extends Component {
             <input type="text" name="phone" onChange={this.handleChange} className="form-control" id="phone" placeholder="phone" />
           </div>
           <div className="form-group">
+            <label htmlFor="phone">Mobile</label>
+            <input type="text" name="mobile" onChange={this.handleChange} className="form-control" id="phone" placeholder="phone" />
+          </div>
+          <div className="form-group">
             <label htmlFor="accountname">Address</label>
-            <input type="text" name="address" onChange={this.handleChange} className="form-control" id="address" placeholder="address" />
+            <input type="text" name="addressline1" onChange={this.handleChange} className="form-control" id="address1" placeholder="Line 1" />
+            <input type="text" name="addressline2" onChange={this.handleChange} className="form-control" id="address2" placeholder="Line 2" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="accountname">Province</label>
+            <select onChange={this.handleprovincechange}>
+              {
+                Object.keys(x).map((ac, k) =>
+                  <option value={ac} key={k}>{ac}</option>
+                )
+              }
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="accountname">District</label>
+            <select onChange={this.handleprovincechange}>
+              {
+                this.state.districts.map((ac, k) =>
+                  <option value={ac} key={k}>{ac}</option>
+                )
+              }
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="accountname">City</label>
+            <input type="city" name="city" onChange={this.handleChange} className="form-control" id="city" placeholder="City" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="accountname">Tole</label>
+            <input type="tole" name="tole" onChange={this.handleChange} className="form-control" id="tole" placeholder="Tole" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="accountname">Country</label>
+            <input type="country" name="country" onChange={this.handleChange} className="form-control" id="email" placeholder="Country" />
           </div>
           <div className="form-group">
             <label htmlFor="accountname">Email</label>
             <input type="email" name="email" onChange={this.handleChange} className="form-control" id="email" placeholder="Email" />
           </div>
           <div className="form-group">
-            <label htmlFor="Principle">Principle</label>
-            <input type="text" name="amount" onChange={this.handleChange} className="form-control" id="Principle" placeholder="Principle" />
+            <label htmlFor="Principle">Father's Name</label>
+            <input type="text" name="fathername" onChange={this.handleChange} className="form-control" id="fathername" placeholder="Father's Name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Principle">Mother's Name</label>
+            <input type="text" name="mothername" onChange={this.handleChange} className="form-control" id="mothername" placeholder="Father's Name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="Principle">Nominiee</label>
+            <select>
+              <option>Husband</option>
+              <option>Wife</option>
+              <option>Father</option>
+              <option>Mother</option>
+              <option>Brother</option>
+              <option>Son</option>
+              <option>daughter</option>
+              <option>Cousin</option>
+            </select>
+            <input type="text" name="nomineefirstname" onChange={this.handleChange} className="form-control" id="nomineefirstname" placeholder="First Name" />
+            <input type="text" name="nomineemiddlename" onChange={this.handleChange} className="form-control" id="nomineemiddlename" placeholder="Middle Name" />
+            <input type="text" name="nomineelastname" onChange={this.handleChange} className="form-control" id="nomineelastname" placeholder="Last Name" />
+            <input type="text" name="nomineecitizennumber" onChange={this.handleChange} className="form-control" id="nomineecitizennumber" placeholder="Citizenship Number" />
+
           </div>
           <div className="form-group row">
             <label className="col-4">Account Type</label>
